bb8545b36f1383bc4e6b1cf3a2be336c1203da78
ANNA-2006 <annamariyashibu2028@cs.sjcetpalai.ac.in>
2025-07-22 22:23:51 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,31 @@
      // TODO: Create a class called Student with name, rollNumber, and grade
 
     // TODO: Create a method to display student details
-public class Main {
-      public static void main(String[] args) {
-        // TODO: Create a Student object and call the method to display details
-    }
+
+     
+ class Student{
+	String name;
+	int rollNumber;
+	char grade;
+	
+	void displayDetails() {
+		System.out.println("Name of the student:"+name);
+		System.out.println("Roll number of the student: "+rollNumber);
+		System.out.println("Grade of the student:"+grade);
+	}
 }
+
+public class Main {
+	public static void main(String[] args) {
+		Student student=new Student();
+		student.name="Anna";
+		student.rollNumber=23;
+		student.grade='A';
+		
+		student.displayDetails();
+	
+	}
+
+}
+
+      
